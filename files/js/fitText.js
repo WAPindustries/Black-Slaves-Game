96c929d5075a49f0871522bc05704a5b160fa682
WAP Industries <116237788+WAPindustries@users.noreply.github.com>
2023-06-07 11:44:41 +0800

    Delete files directory

--- files/js/fitText.js
+++
@@ -1,7 +0,0 @@
-function FitText(div, size){
-    if (size) div.style.fontSize = size
-
-    div.style.fontSize = parseFloat(div.style.fontSize)-1
-    
-    if (div.scrollHeight > div.clientHeight) FitText(div)
-}
